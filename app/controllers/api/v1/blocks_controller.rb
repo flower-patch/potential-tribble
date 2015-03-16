1edfecc2d32e19ce3871f89f8d0a28c1d911f68a
C Haden <chaden@spoonflower.com>
2015-03-16 13:44:29 -0400

    api takes params to select differen SVG files. starting on designs part of API

--- app/controllers/api/v1/blocks_controller.rb
+++ app/controllers/api/v1/blocks_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::BlocksController < ApplicationController
 
   def index
-    svg = Block.new
+    svg = Block.new( params[:block_id] )
 
     render xml: svg.getXML
   end
--- app/models/block.rb
+++ app/models/block.rb
@@ -1,9 +1,13 @@
 class Block
 
+  def initialize( block_id )
+    @block_id = block_id
+  end
+
   def getXML
     xml = ""
 
-    File.open("#{Rails.root}/app/assets/blocks/test.svg") do |f|
+    File.open("#{Rails.root}/app/assets/blocks/test#{@block_id}.svg") do |f|
       xml += f.read
     end
 
