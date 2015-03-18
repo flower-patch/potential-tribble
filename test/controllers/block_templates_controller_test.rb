267cae84d4c4c41aeb51b42120cac5b7f717a095
Topstitch <kallen@spoonflower.com>
2015-03-18 11:28:06 -0400

    Got rid of tests that are no longer relevant

--- test/controllers/block_templates_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class BlockTemplatesControllerTest < ActionController::TestCase
-  test "should get choose_block" do
-    get :choose_block
-    assert_response :success
-  end
-
-end
