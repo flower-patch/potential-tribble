c53f07160fbb432d408704727b349be132b2db5d
C Haden <chaden@spoonflower.com>
2015-03-19 19:34:44 -0400

    fixed test2.svg

--- app/models/quilt.rb
+++ app/models/quilt.rb
@@ -7,4 +7,21 @@ class Quilt < ActiveRecord::Base
     # return doc.xpath("//path")
     doc["svg"]
   end
+
+  def self.get_spoonflower_images( search_term )
+    ids = []
+    urls = []
+
+    response = HTTParty.get("http://api.v1.spoonflower.com/design/search?q=#{search_term}",
+      {:basic_auth => {:username => "demo",:password => "irondemo"}})
+
+    response
+    # response['results'][0]['results'].each do |result|
+    #   ids << result['id']
+    #   urls << result['thumbnail_url']
+    # end
+
+    # return urls
+  end
+
 end
