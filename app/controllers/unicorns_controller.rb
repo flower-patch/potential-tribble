6a31531a575265b0739a66ba57f8670c98bd2ef4
Topstitch <kallen@spoonflower.com>
2015-03-19 16:18:35 -0400

    Nokogiri and unicorns

--- app/controllers/unicorns_controller.rb
+++ app/controllers/unicorns_controller.rb
@@ -1,4 +1,6 @@
 class UnicornsController < ApplicationController
   def index
+    @doc = Nokogiri::XML("<potato><tomato>Dingleberry</tomato></potato>")
+    @rects = @doc.xpath("//tomato")
   end
 end
