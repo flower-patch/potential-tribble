56e93d437d8bd8cbd0ea0e60e833c1120a109114
C Haden <chaden@spoonflower.com>
2015-03-19 16:19:18 -0400

    SVG parsing play

--- app/models/quilt.rb
+++ app/models/quilt.rb
@@ -1,3 +1,10 @@
 class Quilt < ActiveRecord::Base
   belongs_to :project_template
+
+  def get_paths
+    # TODO: get this to properly return paths from SVG
+    doc = Nokogiri::XML(self.svg)
+    # return doc.xpath("//path")
+    doc["svg"]
+  end
 end
