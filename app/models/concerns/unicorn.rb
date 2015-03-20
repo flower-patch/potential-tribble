1256c61767e514499cc1e6adb0b6736e0d520dc4
Topstitch <kallen@spoonflower.com>
2015-03-20 17:16:44 -0400

    Mason helped

---
+++ app/models/concerns/unicorn.rb
@@ -0,0 +1,17 @@
+class Unicorn
+  attr_reader :svg, :paths
+
+  def initialize(svg)
+    @svg = Nokogiri::XML(svg)
+    @paths = @svg.xpath('//*[contains(@style,"fill")]')
+  end
+
+  def all_path_ids
+    @paths.map {|p| p[:id]}
+  end
+
+  def first_paths_style_split_up
+    @paths.first["style"].split(";")
+  end
+
+end
