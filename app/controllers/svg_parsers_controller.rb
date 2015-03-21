8a52ad7881c1b1f7e3182ee027af7e2f184b83b0
Topstitch <kallen@spoonflower.com>
2015-03-21 11:57:50 -0400

    reads from file

--- app/controllers/svg_parsers_controller.rb
+++ app/controllers/svg_parsers_controller.rb
@@ -1,7 +1,12 @@
 class SvgParsersController < ApplicationController
   def index
     #WILL NOT BE THE FIRST QUILT's SVG ALWAYS...JUST TESTING
-    @svg_parser = SvgParser.new(Quilt.first.svg)
+
+    # @svg_parser = SvgParser.new(Quilt.first.svg)
+    f = File.open("#{Rails.root}/app/assets/blocks/test2.svg")
+    @svg_parser = SvgParser.new(f.read)
+    f.close
+
   end
 
 
