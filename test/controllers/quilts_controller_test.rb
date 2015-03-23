82b68758af9ac561594239e3723da42e39f8a731
Topstitch <kallen@spoonflower.com>
2015-03-23 16:37:01 -0400

    final save

--- test/controllers/quilts_controller_test.rb
+++ test/controllers/quilts_controller_test.rb
@@ -31,39 +31,21 @@ class QuiltsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  # lalala
-  #
-  #
-  #
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  #   assert_not_nil assigns(:developers)
-  # end
-  #
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
-  #
-  # test "should create developer" do
+  # test "should create quilt" do
   #   assert_difference('Developer.count') do
   #     post :create, developer: { name: "Johnny Justice", email: "johnny@dev.com", password: "password"}
   #   end
   #   assert_redirected_to developers_path
   # end
   #
-
   # test "should update developer" do
   #   patch :update, id: @developer, developer: { name: "John B. Justice", email: "johnny@dev.com", password: "password"}
   #   assert_redirected_to developers_path
   # end
-  #
-  # test "should destroy developer" do
-  #   assert_difference('Developer.count', -1) do
-  #     delete :destroy, id: @developer
-  #   end
-  #   assert_redirected_to developers_path
+
+  # test "should destroy quilt" do
+  #   until we have quilts assigned to users, no one can delete the quilts that have been saved.
+  #   you can clear your screen during the creation process, but if you approve the quilt, it will go in the database.
   # end
 
 end
