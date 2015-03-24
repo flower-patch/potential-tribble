6e3f8ba875b2f21a3b8fd7ccfa326cd2d74b4399
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-03-24 19:50:16 -0400

    called placeholder api fake thing, fixed a few bugs

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -229,17 +229,47 @@ $(function () {
     console.log(newSvg);
   });
 
+  // The API is on heroku currently, and does not enable cross origin resource sharing (CORS).
+  // This means that a different origin (aka our localhost) cannot access your API.
+
+  // This can be fixed by:
+  // A) Put the api on our localhost
+  // B) enable cross origin resource sharing (CORS) on the api on heroku
+
+  // Additionally, no more newlines.
+  // kthxbai.
+
 
   $('.open-fabric-modal-btn').on('click', function () {
+    // Api.getDesignList().done(function(response) {
+      // var results = response.results[0].results;
+      // var designItem = JSON.parse(DESIGN_ITEM);
+      var results = [DESIGN_ITEM];
+
+      var resultElements = results.map(function(designItem) {
+        var img = $("<img>");
+        img.attr('data-id', designItem.id);
+        img.attr('src', designItem.thumbnail_url);
+
+        var li = $('<li>');
+        li.addClass('fabric-preview');
+        li.append(img);
+
+        return li;
+      });
+
+      $('.fabric-modal-list').empty().append(resultElements);
+    // })
     $('.fabric-modal').toggleClass('show');
     previewQuilt();
     drawPalette('.current-palette', palette);
   });
 
+  $('.fabric-modal-box').on('click', function(e) {
+    e.stopPropagation();
+  });
+
   $('.close-fabric-modal-btn, .fabric-modal').on('click', function () {
-    $('.fabric-modal-box').on('click', function(e) {
-      e.stopPropagation();
-    });
     $('.fabric-modal').toggleClass('show');
     // Once the modal closes, move the svg-editor element back into its original
     // area (.svg-editor-parent), in the main content.
