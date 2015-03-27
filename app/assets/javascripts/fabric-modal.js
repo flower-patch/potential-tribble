aa7f6fe1861efa2351f19c9238dde59a454e968a
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-03-27 12:54:22 -0400

    added js stuff for merge

--- app/assets/javascripts/fabric-modal.js
+++ app/assets/javascripts/fabric-modal.js
@@ -147,14 +147,20 @@ $(function() {
     previewQuilt();
     drawPalette('.current-palette', palette);
   });
-});
-
-
-
-
-
 //////////////////////////////////////////////////////////////////////////////
-// COLOR PICKER
+//Color Picker
 
-// $('.color-search-bar').colorpicker();
-// });
+  $('#picker').colpick({
+    layout:'hex',
+    submit:0,
+    colorScheme:'dark',
+    onChange:function(hsb,hex,rgb,el,bySetColor) {
+      $(el).css('border-color','#'+hex);
+      // Fill the text box just if the color was set using the picker, and not the colpickSetColor function.
+      if(!bySetColor) $(el).val(hex);
+    }
+  }).keyup(function(){
+    $(this).colpickSetColor(this.value);
+  });
+
+});
